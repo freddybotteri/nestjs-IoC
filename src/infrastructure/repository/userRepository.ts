1241cecea28fbcc2158c859ea4603a2390cac361
freddybotterievoluciona <freddy.botteri@evoluciona.com>
2020-04-01 20:08:14 +0200

    change in converter and repository

---
+++ src/infrastructure/repository/userRepository.ts
@@ -0,0 +1,18 @@
+import {repositoryI} from "../../domain/ports/secondary/repositoryI";
+import {Injectable} from "@nestjs/common";
+import { Repository } from "typeorm";
+import {User} from "./entitys/user.entity";
+import {InjectRepository} from "@nestjs/typeorm";
+
+@Injectable()
+export class UserRepository implements repositoryI {
+
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+    ) {}
+
+    getAllUser:any =()=> {
+        return this.usersRepository.find();
+    };
+}
